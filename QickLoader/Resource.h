32bcc2ad52010daffa1689fea338f69a96aa6680
Vic P <vic4key@gmail.com>
2021-07-29 01:45:46 +0700

    Initial

--- QickLoader/Resource.h
+++ QickLoader/Resource.h
@@ -2,20 +2,24 @@
 // Microsoft Visual C++ generated include file.
 // Used by QickLoader.rc
 //
-#define IDR_MAINFRAME					128
-#define IDM_ABOUTBOX					0x0010
-#define IDD_ABOUTBOX					100
-#define IDS_ABOUTBOX					101
-#define IDD_QICKLOADER_DIALOG				102
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_QICKLOADER_DIALOG           102
+#define IDR_MAINFRAME                   128
+#define IDC_PATH                        1000
+#define IDC_DIR                         1001
+#define IDC_TREE                        1002
+#define IDC_AUTO                        1003
+#define ID_LAUNCH                       1004
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
