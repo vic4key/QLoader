aa78195c0a43e01ad81bbcecdb7a2f011b0e4e15
Vic P <vic4key@gmail.com>
2021-08-06 22:41:11 +0700

    Initial

--- QickLoader/QickLoaderDlg.cpp
+++ QickLoader/QickLoaderDlg.cpp
@@ -12,7 +12,13 @@
 
 #include <fstream>
 #include <nlohmann/json.hpp>
-using json = nlohmann::json;
+#include <nlohmann/fifo_map.hpp>
+
+// using json = nlohmann::json; // ordered
+
+template<class K, class V, class dummy_compare, class A>
+using unordered_fifo_map = nlohmann::fifo_map<K, V, nlohmann::fifo_map_compare<K>, A>;
+using json = nlohmann::basic_json<unordered_fifo_map>;
 
 static json g_jdata;
 
