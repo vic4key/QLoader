afad0f7e024dc660b2205c5c8dbf1502408fa24b
Vic P <vic4key@gmail.com>
2022-06-24 13:51:33 +0700

    Initial

--- QLoader/types.h
+++ QLoader/types.h
@@ -1,20 +1,7 @@
 #pragma once
 
 #include <fstream>
-#include <nlohmann/json.hpp>
-#include <nlohmann/fifo_map.hpp>
-
-// using json = nlohmann::json; // ordered
-
-template<class K, class V, class dummy_compare, class A>
-using unordered_fifo_map = nlohmann::fifo_map<K, V, nlohmann::fifo_map_compare<K>, A>;
-using json = nlohmann::basic_json<unordered_fifo_map>;
-
-template <typename value_t>
-value_t json_get(const json& jobject, const std::string& name, const value_t def)
-{
-  return jobject.contains(name) ? jobject[name].get<value_t>() : def;
-}
+#include <3rd-json-fifo-nlohmann/json-fifo-nlohmann.h>
 
 template <class T>
 struct PEB_T
